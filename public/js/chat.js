20519c46bf2dd91eff8d35dda0fc623bb29fa6e8
Paulo Lourenco <paulo@lourenco.net.au>
2019-05-18 14:44:53 +1000

    automatic scrolling

--- public/js/chat.js
+++ public/js/chat.js
@@ -15,6 +15,35 @@ const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 // Options
 const {username, room} = Qs.parse(location.search, { ignoreQueryPrefix: true })
 
+// autoscrolling logic
+const autoscroll = () => {
+    // new message element
+    const $newMessage = $messages.lastElementChild
+
+    // height of the new message
+    const newMessageStyles = getComputedStyle($newMessage)
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
+
+    // visible height
+    const visibleHeight = $messages.offsetHeight
+
+    // height of messages container
+    const containerHeight = $messages.scrollHeight
+
+    // how far have I scrolled?
+    const scrollOffset = $messages.scrollTop + visibleHeight
+
+    if (containerHeight - newMessageHeight <= scrollOffset) {
+        $messages.scrollTop = $messages.scrollHeight
+        // ps - all this code is to provide nice feature of scrolling
+        // to bottom only if the user is looking at last message.
+        // ie - if user is browsing chat history, then it won't 
+        // autoscroll to the bottom. If we weren't providing the feature
+        // we would simply use the above single line of code only!
+    }
+}
+
 // Listeners
 socket.on('message', (msg) => {
     console.log(msg)
@@ -24,6 +53,7 @@ socket.on('message', (msg) => {
         createdAt: moment(msg.createdAt).format('D MMM YY - h:mm a')
     })
     $messages.insertAdjacentHTML('beforeend', html)
+    autoscroll()
 })
 
 socket.on('locationMessage', (msg) => {
@@ -34,6 +64,7 @@ socket.on('locationMessage', (msg) => {
         createdAt: moment(msg.createdAt).format('D MMM YY - h:mm a')
     })
     $messages.insertAdjacentHTML('beforeend', html)
+    autoscroll()
 })
 
 socket.on('roomData', ({ room, users }) => {
